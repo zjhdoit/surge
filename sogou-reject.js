dda83f7b6019ba6280391ceaaf10ff7f520b94ea
ZhangSan <zjhdoit@163.com>
2023-04-17 22:48:09 +0800

    Update and rename sogou-reject417_1.js to sogou-reject.js

--- sogou-reject.js
+++ sogou-reject.js
@@ -1,4 +1,4 @@
 #!name=禁止搜狗输入法访问网络
 #!desc=禁止搜狗输入法访问网络
 [Rule]
-PROCESS-NAME,/Library/Input Methods/SogouInput.app/*,REJECT
+PROCESS-NAME,/Library/Input Methods/SogouInput.app/Contents/SogouServices,REJECT
